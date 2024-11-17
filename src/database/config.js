59490cdcbcb4635c6f26685265008bec68db0e1e
Grazielle Nascimento <grazigrazielle454@gmail.com>
2024-11-17 19:13:57 -0300

    Add database configuration using Sequelize and dotenv

---
+++ src/database/config.js
@@ -0,0 +1,26 @@
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+// Carrega as variáveis de ambiente do arquivo .env
+dotenv.config();
+
+// Definindo a conexão com o banco de dados usando as variáveis de ambiente
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    logging: false
+});
+
+// Testando a conexão com o banco de dados
+sequelize.authenticate()
+    .then(() => {
+        console.log('Conexão com o banco de dados estabelecida com sucesso');
+    })
+    .catch((error) => {
+        console.log('Erro ao conectar ao banco de dados', error);
+    });
+
+
+// Exportando o sequelize e os modelos
+export default sequelize;
