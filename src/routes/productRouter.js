034df61e7b5747280a8579e0ed147ade4f851cc5
Grazielle Nascimento <grazigrazielle454@gmail.com>
2024-11-17 20:12:11 -0300

    Add product router with CRUD routes for product management

---
+++ src/routes/productRouter.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {
+  createProduct,
+  getProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+} from '../controllers/productController.js';
+
+const router = express.Router();
+
+router.post('/product', createProduct);
+router.get('/product', getProducts);
+router.get('/product/:id', getProductById);
+router.put('/product/:id', updateProduct);
+router.delete('/product/:id', deleteProduct);
+
+export default router;
