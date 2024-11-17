52e1635d779f58436f0ed8e7a0666fd2a8ee3802
Grazielle Nascimento <grazigrazielle454@gmail.com>
2024-11-17 19:14:46 -0300

    Add Customer model with associations for Sequelize

---
+++ src/models/Customer.js
@@ -0,0 +1,33 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../database/config.js";
+
+const Customer = sequelize.define('Customer', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    phone: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    address: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+})
+
+Customer.associate = (models) => {
+    Customer.hasMany(models.Appointment, {foreignKey: 'customerId' });
+}
+
+export default Customer;
