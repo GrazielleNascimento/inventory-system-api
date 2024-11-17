db1729a5ab9dfd7d96bcf5cc72f4e44c71650e3b
Grazielle Nascimento <grazigrazielle454@gmail.com>
2024-11-17 19:13:49 -0300

    Initialize server with Express and set up API routes

---
+++ server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+//import cors from 'cors';
+import sequelize from './src/database/config.js'
+//import swaggerConfig from './src/
+import professionalRoutes from './src/routes/professionalRoutes.js';
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+/* app.use(cors({
+    origin: 'http://localhost:5500',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+})) */
+
+//swaggerConfig(app);
+
+// Rotas da API
+app.use('/api', professionalRoutes);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the API');
+});
+
+ sequelize.sync({force: false})
+.then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on PORT ${port}`);
+    });
+})
+.catch((error) => {
+    console.log('Erro ao conectar ao banco de dados', error);
+}); 
+
+
