240bbc4ac55a418027545c4db238a41c4e37cbdd
Grazielle Nascimento <grazigrazielle454@gmail.com>
2024-11-17 19:15:29 -0300

    Add Service model with associations for Sequelize

---
+++ src/models/Service.js
@@ -0,0 +1,33 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../database/config.js";
+
+const Service = sequelize.define('Service', { 
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    price : {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+   duration: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+   
+});
+
+Service.associate = (models) => {
+    Service.hasMany(models.Appointment, {foreignKey: 'serviceId'});
+};
+
+export default Service;
