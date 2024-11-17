020fb2ec0955371db0e407d98a24fa21462ce9a4
Grazielle Nascimento <grazigrazielle454@gmail.com>
2024-11-17 20:11:59 -0300

    Add category router with CRUD routes for category management

---
+++ src/routes/categoryRouter.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {
+  createCategory,
+  getCategories,
+  getCategoryById,
+  updateCategory,
+  deleteCategory,
+} from '../controllers/categoryController.js';
+
+const router = express.Router();
+
+router.post('/category', createCategory);
+router.get('/category', getCategories);
+router.get('/category/:id', getCategoryById);
+router.put('/category/:id', updateCategory);
+router.delete('/category/:id', deleteCategory);
+
+export default router;
